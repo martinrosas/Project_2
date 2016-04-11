07fda4bfb76a27ce5abe711c08c5165bd8e3e85b
Martin Rosas <martinleonrosas@gmail.com>
2016-04-11 15:58:10 -0700

    Movie controller working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,17 +4,39 @@ class MoviesController < ApplicationController
 end
 
   def show
+  @movie = Movie.find(params[:id])
+end
+
+  def new
+  @movie = Movie.new
   end
 
-  def newcreate
+  def create
+  @movie = Movie.new(params.require(:movie).permit(:title, :description, :runtime, :genre))
+
+  if @movie.save
+    redirect_to movies_path
+  else
+    render :new
   end
+end
 
   def edit
+  @movie = Movie.find(params[:id])
   end
 
   def update
-  end
+  @movie = Movie.find(params[:id])
 
+  if @movie.update_attributes(params.require(:movie).permit(:title, :description, :runtime, :genre))
+    redirect_to movies_path
+  else
+    render :edit
+  end 
+  end
   def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
   end
 end
