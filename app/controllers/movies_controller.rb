c50aa6a14324c6b0e563fb065c6979c31f79d7e5
martin rosas <martinleonrosas@gmail.com>
2016-04-14 20:17:37 -0700

    Removed option to delete movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,43 +2,44 @@ class MoviesController < ApplicationController
   def index
   @movies = Movie.all
 
-end
+  end
 
   def show
   @movie = Movie.find(params[:id])
-end
-
-  def new
-  @movie = Movie.new
-  end
-
-  def create
-  @movie = Movie.new(params.require(:movie).permit(:title, :description, :runtime, :genre))
-
-  if @movie.save
-    redirect_to movies_path
-  else
-    render :new
   end
 end
 
-  def edit
-  @movie = Movie.find(params[:id])
-  end
+#   def new
+#   @movie = Movie.new
+#   end
 
-  def update
-  @movie = Movie.find(params[:id])
+#   def create
+#   @movie = Movie.new(params.require(:movie).permit(:title, :description, :runtime, :genre))
 
-  if @movie.update_attributes(params.require(:movie).permit(:title, :description, :runtime, :genre))
-    redirect_to movies_path
-  else
-    render :edit
-  end
-  end
-  def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
-    redirect_to movies_path
-  end
+#   if @movie.save
+#     redirect_to movies_path
+#   else
+#     render :new
+#   end
+# end
 
-end
+#   def edit
+#   @movie = Movie.find(params[:id])
+#   end
+
+#   def update
+#   @movie = Movie.find(params[:id])
+
+#   if @movie.update_attributes(params.require(:movie).permit(:title, :description, :runtime, :genre))
+#     redirect_to movies_path
+#   else
+#     render :edit
+#   end
+#   end
+#   def destroy
+#     @movie = Movie.find(params[:id])
+#     @movie.destroy
+#     redirect_to movies_path
+#   end
+
+# end
